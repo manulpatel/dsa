f4ce7eab0adadcac730e180f2172737018d8d13b
Manul Patel <manulpatel99@gmail.com>
2024-12-25 23:51:35 +0530

    Day3: Only CP Ladders

---
+++ Codeforces/110A - Nearly Lucky Number.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+
+	int min = INT_MAX, max = INT_MIN;
+	int minI, maxI;
+
+	for (int i = 1; i < n; i++)
+	{
+		int a;
+		cin >> a;
+
+		if (a > max)
+		{
+			max = a;
+			maxI = i;
+		}
+		if (a < min)
+		{
+			min = a;
+			minI = i;
+		}
+	}
+
+	if (maxI < minI)
+	{
+		cout << maxI - 1 + n - minI;
+	}
+	else
+	{
+		cout << n - minI - 2 + maxI;
+	}
+
+	return 0;
+}
